eca0df2050f7c9d972acbb2a3df06e5943dfab97
Shoaib <shoaibmir236@gmail.com>
2022-03-23 15:01:09 +0500

    added changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+import { hydrate, render } from "react-dom";
+
+const APP = (
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  </BrowserRouter>
+)
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );

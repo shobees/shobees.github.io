cd3e20a9d7ca5d451ec9c17c7731555ad29a21c7
Shoaib <shoaibmir236@gmail.com>
2022-03-23 16:07:36 +0500

    added changes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import MetaDecorator from "../../Components/MetaDecorator";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, []);
+  console.log("response", posts)
   return (
     <Fragment>
       <MetaDecorator
-        title="title of the card"
-        description="description of the card"
+        title={posts.length && posts[0].title}
+        description={posts.length && posts[0].body}
         imageUrl="https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g"
       />
       <div>Home</div>;
